2f321f3029a2ada3a93955cfba3d7b0946305aee
viju-mathew <vijujm@gmail.com>
2014-02-26 02:51:35 -0500

    added main class and moveable interface

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+import view.Workspace;
+
+public class Main {
+
+	private static void createAndShowMainWindow() {
+		
+		JFrame frame = new JFrame("SLOGO");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Container mainWindow = new Workspace();
+		frame.add(mainWindow);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		createAndShowMainWindow();
+		
+    }
+	
+}
