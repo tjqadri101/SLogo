21435f3adfc8efcb8ea821a82c0d3b1c2a7c0fe5
taragu <tara.weiqing@gmail.com>
2014-02-28 09:30:07 -0500

    reduced duplicated code in createTree

--- src/parse/Parser.java
+++ src/parse/Parser.java
@@ -84,77 +84,67 @@ public class Parser {
 
     }
 
-
     public AbstractNode createTree(Function function, Turtle turtle) throws ClassNotFoundException, 
-                                            NoSuchMethodException, SecurityException, InstantiationException, 
-                                            IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
-            myTurtle = turtle;
-            NodeFactory nodeFactory = new NodeFactory(myTurtle);
-            AbstractNode root = new BlockNode(myTurtle);
-            // getting rid of initial spaces and outside brackets
-            int beginIndex = 0;
-            while (function.getContent().charAt(beginIndex)==' ' || function.getContent().charAt(beginIndex)=='[') {
-                beginIndex ++;
-            }
-            int endIndex = function.getContent().length() - 1;
-            while(function.getContent().charAt(endIndex) == ' ' ||  function.getContent().charAt(endIndex)=='[') {
-                endIndex --;
-            }
-            function.setContent(function.getContent().substring(beginIndex, endIndex + 1));
+    NoSuchMethodException, SecurityException, InstantiationException, 
+    IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException {
+        myTurtle = turtle;
+        NodeFactory nodeFactory = new NodeFactory(myTurtle);
+        AbstractNode root = new BlockNode(myTurtle);
+        processFunction(function);
 
-            String[] words = function.getContent().split(" ");
-            Queue<String> queue = new LinkedList<String>();
-            for (String word : words) {
-                queue.add(word);
-            }
-            String currentWord = queue.poll();
-            AbstractNode currentNode = nodeFactory.createNode(currentWord);
-            root.setLeftNode(currentNode);
+        String[] words = function.getContent().split(" ");
+        Queue<String> queue = new LinkedList<String>();
+        for (String word : words) {
+            queue.add(word);
+        }
+        String currentWord = queue.poll();
+        AbstractNode currentNode = nodeFactory.createNode(currentWord);
+        root.setLeftNode(currentNode);
 
-            while (!queue.isEmpty()) {
-                if (currentNode instanceof NumberNode ||
-                        currentNode instanceof VariableNode) {
-                    // return to parent
+        while (!queue.isEmpty()) {
+            if (currentNode instanceof NumberNode ||
+                    currentNode instanceof VariableNode) {
+                // return to parent
+                currentNode = currentNode.getParent();
+                if (!currentNode.allowsTwoChildren() || 
+                        (currentNode.allowsTwoChildren() && currentNode.getChildren().size()==2)) {
                     currentNode = currentNode.getParent();
-                    if (!currentNode.allowsTwoChildren() || 
-                            (currentNode.allowsTwoChildren() && currentNode.getChildren().size()==2)) {
-                        currentNode = currentNode.getParent();
-                    }
                 }
+            }
 
-                if (currentNode instanceof IfElseNode) {
-                    // create two block nodes
-                    AbstractNode bnLeft = new BlockNode(myTurtle);
-                    AbstractNode bnRight = new BlockNode(myTurtle);
-                    currentNode.setLeftNode(bnLeft);
-                    currentNode.setRightNode(bnRight);
-                    // create a condition node for left block
-                    AbstractNode conditionLeft = new ConditionNode(myTurtle);
-                    bnLeft.setLeftNode(conditionLeft);
-                    bnLeft.setRightNode(new BlockNode(myTurtle));
+            if (currentNode instanceof IfElseNode) {
+                // create two block nodes
+                AbstractNode bnLeft = new BlockNode(myTurtle);
+                AbstractNode bnRight = new BlockNode(myTurtle);
+                currentNode.setLeftNode(bnLeft);
+                currentNode.setRightNode(bnRight);
+                // create a condition node for left block
+                AbstractNode conditionLeft = new ConditionNode(myTurtle);
+                bnLeft.setLeftNode(conditionLeft);
+                bnLeft.setRightNode(new BlockNode(myTurtle));
 
-                    // create condition for condition left; condition right is the opposite of condition left
-                    currentNode = bnLeft.getLeftNode();
-                }
-                if ((currentNode instanceof IfNode || currentNode instanceof RepeatNode) && currentNode.getLeftNode() == null) {
-                    // create 1 block node
-                    AbstractNode bn = new BlockNode(myTurtle);
-                    // create 1 condition node
-                    AbstractNode conditionNode = new ConditionNode(myTurtle);
-                    currentNode.setLeftNode(conditionNode);
-                    currentNode.setRightNode(bn);
-                    currentNode = conditionNode;
-                }
+                // create condition for condition left; condition right is the opposite of condition left
+                currentNode = bnLeft.getLeftNode();
+            }
+            if ((currentNode instanceof IfNode || currentNode instanceof RepeatNode) && currentNode.getLeftNode() == null) {
+                // create 1 block node
+                AbstractNode bn = new BlockNode(myTurtle);
+                // create 1 condition node
+                AbstractNode conditionNode = new ConditionNode(myTurtle);
+                currentNode.setLeftNode(conditionNode);
+                currentNode.setRightNode(bn);
+                currentNode = conditionNode;
+            }
 
-                String nextWord = queue.poll();
-                if (nextWord == null) {
-                    return root;
-                }
+            String nextWord = queue.poll();
+            if (nextWord == null) {
+                return root;
+            }
+            for (int i=0;i<2;i++) {
                 if (nextWord.equals("[")) {
                     // if the parent node is a repeat node or an if node, or the parent of the parent node is an if else node
-                    if (currentNode.getParent() instanceof RepeatNode || currentNode.getParent() instanceof IfNode) {
-                        currentNode = currentNode.getParent().getRightNode(); // go to block
-                    } else if (currentNode.getParent().getParent() instanceof IfElseNode) {
+                    if (currentNode.getParent() instanceof RepeatNode || currentNode.getParent() instanceof IfNode ||
+                            currentNode.getParent().getParent() instanceof IfElseNode) {
                         currentNode = currentNode.getParent().getRightNode(); // go to block
                     } else if (currentNode instanceof RepeatNode || currentNode instanceof IfNode) {
                         currentNode = currentNode.getRightNode(); // go to block
@@ -175,63 +165,38 @@ public class Parser {
                         return root;
                     }
                 }
-
-
-                
-                //DUPLICATED CODE
-                if (nextWord.equals("[")) {
-                    // if the parent node is a repeat node or an if node, or the parent of the parent node is an if else node
-                    if (currentNode.getParent() instanceof RepeatNode || currentNode.getParent() instanceof IfNode) {
-                        currentNode = currentNode.getParent().getRightNode(); // go to block
-                    } else if (currentNode.getParent().getParent() instanceof IfElseNode) {
-                        currentNode = currentNode.getParent().getRightNode(); // go to block
-                    } else if (currentNode instanceof IfElseNode && currentNode.getLeftNode().getChildren().size()>=2) {
-                        // ifelse node's left block is completed --> go to right block
-                        currentNode = currentNode.getRightNode();
-                    }
-                    nextWord = queue.poll();
-                }
-
-                if (nextWord.equals("]")) {    
-                    if (currentNode == null) {
-                        return root;
-                    }
-                    currentNode = currentNode.getParent();
-                    if (currentNode.getParent() instanceof IfElseNode) {
-                        currentNode = currentNode.getParent();
-                    }
-                    nextWord = queue.poll();
-                    if (nextWord == null) {
-                        return root;
-                    }
-                }
-                
-                AbstractNode nextNode = nodeFactory.createNode(nextWord);
-                
-                if (currentNode.getLeftNode()== null) {
-                    currentNode.setLeftNode(nextNode);
-                } else if (currentNode.getRightNode() == null && currentNode.allowsTwoChildren()) {
-                    currentNode.setRightNode(nextNode);
-                } else if (currentNode.getRightNode() != null && currentNode.allowsMoreThanTwoChildren()){
-                    currentNode.addChild(nextNode);
-                }
-
-                currentNode = nextNode;
-                currentWord = nextWord;
-
             }
-            return root;
+            AbstractNode nextNode = nodeFactory.createNode(nextWord);
+
+            if (currentNode.getLeftNode()== null) {
+                currentNode.setLeftNode(nextNode);
+            } else if (currentNode.getRightNode() == null && currentNode.allowsTwoChildren()) {
+                currentNode.setRightNode(nextNode);
+            } else if (currentNode.getRightNode() != null && currentNode.allowsMoreThanTwoChildren()){
+                currentNode.addChild(nextNode);
+            }
+
+            currentNode = nextNode;
+            currentWord = nextWord;
+
+        }
+        return root;
 
     }
 
-    private AbstractNode checkForBrackets() {
-        //TODO
-        
-        return null;
+    private void processFunction (Function function) {
+        // getting rid of initial spaces and outside brackets
+        int beginIndex = 0;
+        while (function.getContent().charAt(beginIndex)==' ' || function.getContent().charAt(beginIndex)=='[') {
+            beginIndex ++;
+        }
+        int endIndex = function.getContent().length() - 1;
+        while(function.getContent().charAt(endIndex) == ' ' ||  function.getContent().charAt(endIndex)=='[') {
+            endIndex --;
+        }
+        function.setContent(function.getContent().substring(beginIndex, endIndex + 1));
     }
-    
-    
-    
+
     public void traverseTree(Turtle turtle, AbstractNode root) {  
         myTurtle = turtle;
         if (root!=null) {
