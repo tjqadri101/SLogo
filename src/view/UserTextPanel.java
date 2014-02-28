36180eaf2deda317a74a466b1207862dac5323ba
Chad Coviel <chadcoviel@wl-10-190-51-203.wireless.duke.edu>
2014-02-28 07:10:41 -0500

    Some additions to UserTextPanel.java
    Added functionality to update a String whenever the user performs a
    keystroke and a get method to return this string so that it may be
    passed for parsing to the backend. Also created a BackendTurtle class
    to replace the previous Turtle class we had.

--- src/view/UserTextPanel.java
+++ src/view/UserTextPanel.java
@@ -5,16 +5,29 @@ import java.awt.event.TextListener;
 
 import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
 public class UserTextPanel extends JTextField implements TextListener{
 	
+	//user input data
+	private String userInputData;
+	
 	public UserTextPanel(int numColumns){
+		//Invokes a JTextField constructor
+		super(numColumns);
+		
+		//sets the text to be left justified
 		setHorizontalAlignment(LEFT);
 	}
 
-	@Override
+	//Updates the contents of userInputData whenever
+	//the user inputs a keystroke
 	public void textValueChanged(TextEvent e) {
-
-		
+		userInputData = paramString();
+	}
+	
+	//return the userInputData
+	public String getInputData(){
+		return userInputData;
 	}
 
 }
