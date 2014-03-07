0cfb38e771ec6080662cef215bfafc440e65e7c2
Benson <benson.tran@duke.edu>
2014-03-07 15:47:09 -0500

    pre-merge. Added controller class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,19 @@
 package controller;
 
+import model.Model;
+import turtle.ITurtle;
+
 public class Controller {
+	
+	private Model myModel;
+	
+	public Controller(){
+		
+	}
+	
+	public ITurtle getBackendTurtle(){
+		return null;
+		
+	}
 
 }
